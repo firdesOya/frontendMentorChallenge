efd7db678af37fe866fd9eab50b9d5f0f94e4816
Firdevs <firdes.sahin92@gmail.com>
2024-10-25 23:21:21 +0300

    navbar hover add

--- loopstudios-landing-page/src/app/components/Header.js
+++ loopstudios-landing-page/src/app/components/Header.js
@@ -6,17 +6,18 @@ export default function Header() {
   return (
     <div className="container mx-auto relative z-10 pt-10 flex items-center justify-between px-6">
       <Link href="#">
-        <img src="/images/logo.svg"/>
+        <img src="/images/logo.svg" />
       </Link>
       <div className="text-white font-alata hidden md:flex">
         {nav.map(item => {
-          return <div key={item.id} className="px-4 py-3">
+          return <div key={item.id} className="px-4 flex flex-col py-3 group">
             <Link href="#" >{item.title}</Link>
+            <span className='w-0 mx-auto rounded-sm h-0.5 bg-white transition-all duration-300 group-hover:w-6 opacity-0 group-hover:opacity-100 '></span>
           </div>
         })}
       </div>
       <div className="block md:hidden">
-        <img src="/images/icon-hamburger.svg"/>
+        <img src="/images/icon-hamburger.svg" />
       </div>
     </div>
   )
--- loopstudios-landing-page/src/app/page.js
+++ loopstudios-landing-page/src/app/page.js
@@ -4,7 +4,7 @@
 export default function Home() {
   return (
     <>
-      <div className="bg-hero  relative -top-[88px]">
+      <div className="bg-hero  fixed w-full top-0">
         <section className="container mx-auto px-6 pt-[380px] pb-[300px] md:pt-[200px] md:pb-[130px]">
           <div className="uppercase text-white font-josefin text-6xl p-5 border-2 border-solid border-white max-w-[600px] md:max-w-[650px] md:text-7xl">
             Immersive Experiences That  Deliver

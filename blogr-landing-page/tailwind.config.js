723eb638902efdb801ea99ba23e8cecc93ab4f44
Firdevs <firdes.sahin92@gmail.com>
2024-11-18 16:39:53 +0300

    sublinks add

--- blogr-landing-page/tailwind.config.js
+++ blogr-landing-page/tailwind.config.js
@@ -15,6 +15,10 @@ module.exports = {
         "very-dark-grayish-blue" : "hsl(207, 13%, 34%)",
         "very-dark-black-blue ": "hsl(240, 10%, 16%)",
       },
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(to right, hsl(13, 100%, 72%), hsl(353, 100%, 62%))',
+        'background-gradient':'linear-gradient(to right, hsl(237, 17%, 21%),hsl(237, 23%, 32%))'
+      },
     },
   },
   plugins: [],

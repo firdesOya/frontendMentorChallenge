3e46b48f9b43853afa6de48bf2515fca13b2a729
Firdevs <firdes.sahin92@gmail.com>
2024-11-20 15:00:20 +0300

    feat: Add Prettier configuration

--- blogr-landing-page/tailwind.config.js
+++ blogr-landing-page/tailwind.config.js
@@ -12,12 +12,14 @@ module.exports = {
         "very-light-red": "hsl(355, 100%, 74%)",
         "very-dark-blue": " hsl(208, 49%, 24%)",
         "grayish-blue": "hsl(240, 2%, 79%)",
-        "very-dark-grayish-blue" : "hsl(207, 13%, 34%)",
+        "very-dark-grayish-blue": "hsl(207, 13%, 34%)",
         "very-dark-black-blue": "hsl(240, 10%, 16%)",
       },
       backgroundImage: {
-        'custom-gradient': 'linear-gradient(to right, hsl(13, 100%, 72%), hsl(353, 100%, 62%))',
-        'background-gradient':'linear-gradient(to right, hsl(237, 17%, 21%),hsl(237, 23%, 32%))'
+        "custom-gradient":
+          "linear-gradient(to right, hsl(13, 100%, 72%), hsl(353, 100%, 62%))",
+        "background-gradient":
+          "linear-gradient(to right, hsl(237, 17%, 21%),hsl(237, 23%, 32%))",
       },
     },
   },

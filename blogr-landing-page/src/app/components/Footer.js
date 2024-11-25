64f0ee71269190b366e52dd91e3992fad9be5d7a
Firdevs <firdes.sahin92@gmail.com>
2024-11-25 15:38:53 +0300

    feat: added footer

--- blogr-landing-page/src/app/components/Footer.js
+++ blogr-landing-page/src/app/components/Footer.js
@@ -1,5 +1,39 @@
 import React from "react";
+import { navList } from "../constant/config";
+import Link from "next/link";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-very-dark-black-blue mt-32 rounded-tr-[100px] h-full w-full">
+      <div className="flex flex-col items-center lg:items-start lg:flex-row max-w-[1140px] px-6 xl:px-0 mx-auto gap-20 py-16 lg:py-[70px] lg:gap-48 ">
+        <div>
+          <img src="/images/logo.svg" />
+        </div>
+        <div className="flex flex-col text-center md:text-start gap-6 md:gap-24 md:flex-row md:items-start  lg:gap-52 ">
+          {navList.map((navItem) => {
+            return (
+              <div key={navItem.id}>
+                <h4 className="text-overpass font-semibold text-base text-white mb-5 lg:mb-8 mt-1">
+                  {navItem.title}
+                </h4>
+                <div className="flex  flex-col gap-4 items-center md:items-start text-overpass font-semibold  text-grayish-blue text-sm ">
+                  {navItem.subLinks.map((subItem) => {
+                    return (
+                      <Link
+                        key={subItem.id}
+                        href={subItem.path}
+                        className="cursor-pointer hover:underline "
+                      >
+                        {subItem.subTitle}
+                      </Link>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </footer>
+  );
 }

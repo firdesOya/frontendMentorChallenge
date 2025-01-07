e9ab073803cb7fcebe9d37c779173492870532a0
Firdevs <firdes.sahin92@gmail.com>
2025-01-07 16:56:56 +0300

    feat : add card menu

---
+++ product-list-with-cart-main/src/app/components/Card.js
@@ -0,0 +1,11 @@
+import React from "react";
+import AddButton from "./AddButton";
+
+export default function Card() {
+  return (
+    <div>
+      Card
+      <AddButton />
+    </div>
+  );
+}
--- product-list-with-cart-main/src/app/page.js
+++ product-list-with-cart-main/src/app/page.js
@@ -1,6 +1,14 @@
+import CardList from "./components/CardList";
 
 export default function Home() {
   return (
-    <></>
+    <div className="bg-rose-50 w-full h-full">
+      <div className="pt-10 max-w-[1220px] min-w-[320px] mx-auto py-[70px] px-5 ">
+        <div>
+          <h2 className="font-semibold text-3xl mb-4">Desserts</h2>
+          <CardList />
+        </div>
+      </div>
+    </div>
   );
 }

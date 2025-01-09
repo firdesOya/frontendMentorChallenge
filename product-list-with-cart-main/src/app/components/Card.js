3c4c4cf2211061b4db123741c26159060b78f2ca
Firdevs <firdes.sahin92@gmail.com>
2025-01-09 16:46:27 +0300

    feat:Add card and cardlist

--- product-list-with-cart-main/src/app/components/Card.js
+++ product-list-with-cart-main/src/app/components/Card.js
@@ -1,11 +1,25 @@
 import React from "react";
 import AddButton from "./AddButton";
+import Image from "next/image";
 
-export default function Card() {
+export default function Card({ item }) {
   return (
     <div>
-      Card
-      <AddButton />
+      <div className="inline-block">
+        <picture className="w-full">
+          <source srcSet={item.imgDesktop} media="(min-width:1024px)" />
+          <source srcSet={item.imgTablet} media="(min-width:768px)" />
+          <img src={item.imgMobile} alt={item.title} />
+        </picture>
+        <div className="flex justify-center -mt-4">
+          <AddButton />
+        </div>
+      </div>
+      <div className="pt-3 flex flex-col items-start gap-1">
+        <span className="text-rose-400">{item.title}</span>
+        <span className="font-semibold text-rose-900">{item.subTitle}</span>
+        <span className="text-red font-semibold">{item.price}</span>
+      </div>
     </div>
   );
 }
--- product-list-with-cart-main/src/app/components/CardList.js
+++ product-list-with-cart-main/src/app/components/CardList.js
@@ -1,32 +1,15 @@
 import React from "react";
 import { CardMenu } from "../constants/config";
-import Image from "next/image";
-import AddButton from "./AddButton";
+import Card from "./Card";
+import OrderButton from "./OrderButton";
 
 export default function CardList() {
   return (
-    <div className="flex flex-wrap gap-5 max-w-[800px] ">
-      {CardMenu.map((item) => {
-        return (
-          <div key={item.id} className="relative">
-            <Image
-              src={item.imgDesktop}
-              alt={item.title}
-              width={250}
-              height={100}
-              className="rounded-md "
-            />
-            <div className="flex items-center justify-center -mt-5 ">
-              <AddButton />
-            </div>
-            <div className="pt-3 flex flex-col items-start gap-1">
-              <span className="text-rose-400">{item.title}</span>
-              <span className="font-semibold text-rose-900">{item.subTitle}</span>
-              <span className="text-red font-semibold">{item.price}</span>
-            </div>
-          </div>
-        );
-      })}
+    <div className="grid grid-col-1 md:grid-col-2 lg:grid-cols-3 gap-2">
+      {CardMenu.map((item) => (
+        <Card key={item.id} item={item} />
+      ))}
+    
     </div>
   );
 }

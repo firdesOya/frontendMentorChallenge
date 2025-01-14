166f7ffcba7c45e861f6a9499cd8324473aeb830
Firdevs <firdes.sahin92@gmail.com>
2025-01-14 22:39:23 +0300

    feat:added onclick

--- product-list-with-cart-main/src/app/components/AddButton.js
+++ product-list-with-cart-main/src/app/components/AddButton.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import React from "react";
 
-export default function AddButton() {
+export default function AddButton({item, addToCart }) {
   return (
-    <button className="flex items-center bg-white text-rose-900 text-xs font-semibold gap-2 border border-rose-500 rounded-full px-6 py-2 hover:border-red hover:text-red">
+    <button 
+    onClick={()=>addToCart(item)}
+    className="flex items-center bg-white text-rose-900 text-xs font-semibold gap-2 border border-rose-500 rounded-full px-8 py-2 hover:border-red hover:text-red">
       <Image
         src="/images/icon-add-to-cart.svg"
         alt="Add to cart"

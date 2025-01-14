c97cfe9c2844a46f7ea665656de0f6e1dcbf221e
Firdevs <firdes.sahin92@gmail.com>
2025-01-14 22:40:05 +0300

    feat:added props

--- product-list-with-cart-main/src/app/components/CardList.js
+++ product-list-with-cart-main/src/app/components/CardList.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { CardMenu } from "../constants/config";
 import Card from "./Card";
-import OrderButton from "./OrderButton";
 
-export default function CardList() {
+
+export default function CardList({addToCart}) {
   return (
-    <div className="grid grid-col-1 md:grid-col-2 lg:grid-cols-3 gap-2">
+    <div className="grid grid-col-1 md:grid-col-2 lg:grid-cols-3 gap-7">
       {CardMenu.map((item) => (
-        <Card key={item.id} item={item} />
+        <Card key={item.id} item={item} addToCart={addToCart} />
       ))}
     
     </div>

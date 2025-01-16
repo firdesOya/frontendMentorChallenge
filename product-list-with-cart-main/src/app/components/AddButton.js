5085c7e68d14a2113974c4b2faf20f2e92ec0d3b
Firdevs <firdes.sahin92@gmail.com>
2025-01-16 22:22:55 +0300

    update

--- product-list-with-cart-main/src/app/components/AddButton.js
+++ product-list-with-cart-main/src/app/components/AddButton.js
@@ -1,18 +1,19 @@
 import Image from "next/image";
 import React from "react";
 
-export default function AddButton({item, addToCart }) {
+export default function AddButton({ item, addToCart }) {
   return (
-    <button 
-    onClick={()=>addToCart(item)}
-    className="flex items-center bg-white text-rose-900 text-xs font-semibold gap-2 border border-rose-500 rounded-full px-8 py-2 hover:border-red hover:text-red">
+    <button
+      onClick={() => addToCart(item)}
+      className="flex items-center bg-white text-rose-900 text-xs font-semibold gap-2 border border-rose-500 rounded-full px-8 py-2 hover:border-red hover:text-red"
+    >
       <Image
         src="/images/icon-add-to-cart.svg"
         alt="Add to cart"
         width={20}
         height={10}
       />
-      Add to Cart
+      <span>Add to Cart</span>
     </button>
   );
 }

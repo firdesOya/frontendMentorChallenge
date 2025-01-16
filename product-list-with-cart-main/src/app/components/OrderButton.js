28c277ec67cfb8bf007cdd0c47800ff91bea0656
Firdevs <firdes.sahin92@gmail.com>
2025-01-16 16:20:41 +0300

    update:width update

--- product-list-with-cart-main/src/app/components/OrderButton.js
+++ product-list-with-cart-main/src/app/components/OrderButton.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function OrderButton({title}) {
   return (
-    <button className="bg-red hover:bg-[#962C0C] text-white text-sm font-semibold px-24 py-4 rounded-full">
+    <button className="bg-red hover:bg-[#962C0C] text-white text-sm max-w-[330px] w-full font-semibold text-center py-4 rounded-full">
       {title}
     </button>
   );
